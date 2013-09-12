bfe9929f14df1d89de09ba2c664a08ffa0f0f7b6
Eirik Lied <eiriklied@gmail.com>
2013-09-12 23:11:29 +0200

    Ready ember app! Just need to uncomment the create call in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require app
 
 // for more details see: http://emberjs.com/guides/application/
-App = Ember.Application.create();
 
-//= require_tree .
+// App = Ember.Application.create({
+//   rootElement: '#ember-app'
+// });
